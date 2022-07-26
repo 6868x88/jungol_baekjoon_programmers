f0376548a69a1008205b41e58b277b1fa2f3ff84
지은 <107034832+6868x88@users.noreply.github.com>
2022-07-26 11:25:21 +0900

    220726
    배열1 159

---
+++ JungolTest/src/Test159.java
@@ -0,0 +1,23 @@
+package Test159;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class Test159 {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int num = s.nextInt();
+		Integer arr[] = new Integer [num];
+		for(int i=0; i<arr.length; i++) {
+			arr[i] = s.nextInt();
+		}
+		Arrays.sort(arr, Collections.reverseOrder());
+
+		for(int i=0; i<arr.length; i++) {
+			System.out.println(arr[i]);
+		}
+		s.close();
+	}
+}
