c623356d74a91816947ae4d8f21751f5b758c4b7
지은 <107034832+6868x88@users.noreply.github.com>
2024-03-10 16:51:18 +0900

    240310_1 음양 더하기

---
+++ programmers/240310_1.java
@@ -0,0 +1,16 @@
+/*
+음양 더하기
+*/
+
+class Solution {
+    public int solution(int[] absolutes, boolean[] signs) {
+        int answer = 0;
+        
+        for(int i=0; i<absolutes.length; i++) {
+			if(signs[i] == false) {absolutes[i] = (-1)*absolutes[i];};
+		
+			answer += absolutes[i];
+		}
+        return answer;
+    }
+}
