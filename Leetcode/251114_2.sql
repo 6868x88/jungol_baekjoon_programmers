629da205bac0448217c3597e3613bf18e29bd0af
지은 <107034832+6868x88@users.noreply.github.com>
2025-11-14 14:59:36 +0900

    251114_2 3475. DNA Pattern Recognition

---
+++ Leetcode/251114_2.sql
@@ -0,0 +1,14 @@
+/*
+3475. DNA Pattern Recognition 
+*/
+
+
+select  s.*
+        , case when dna_sequence like 'ATG%' then 1 else 0 end as has_start
+        , case when dna_sequence like '%TAA' then 1 
+               when dna_sequence like '%TAG' then 1
+               when dna_sequence like '%TGA' then 1
+               else 0 end as has_stop   
+        , case when dna_sequence like '%ATAT%' then 1 else 0 end as has_atat
+        , case when dna_sequence like '%GGG%' then 1 else 0 end as has_ggg
+from Samples s
