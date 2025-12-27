65ca4622ea9e48bab98d400098bbee5c046f9c73
지은 <107034832+6868x88@users.noreply.github.com>
2025-12-27 11:34:47 +0900

    251227_1.java 15. 3Sum

---
+++ Leetcode/251227_1.java
@@ -0,0 +1,30 @@
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+		
+		List<List<Integer>> answer = new ArrayList<>();
+		Set<List<Integer>> set = new HashSet<>();
+		
+		
+		for(int i=0; i<nums.length; i++) {
+			int j = i + 1;
+			int k = nums.length - 1;
+			while (j < k) {
+				int sum = nums[i] + nums[j] + nums[k];
+				
+				if(sum == 0) {
+					set.add(Arrays.asList(nums[i], nums[j], nums[k]));
+					j++;
+					k--;
+				}else if(sum < 0) {
+					j++;
+				}else {
+					k--;
+				}
+			}
+		}
+		
+		answer.addAll(set);
+		return answer;
+    }
+}
