5360d651a2c04b8083c8f1b4b4fa6fdec396c867
지은 <107034832+6868x88@users.noreply.github.com>
2025-12-29 12:06:42 +0900

    251229_1.java 소인수분해

---
+++ programmers/251229_1.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int n) {
+		Set<Integer> set = new HashSet<Integer>();
+
+		for (int i = 2; i * i <= n; i++) {
+			while (n % i == 0) {
+				set.add(i);
+				n /= i;
+			}
+		}
+
+		if (n > 1) {set.add(n);}
+		int[] answer = set.stream()
+						  .sorted()
+						  .mapToInt(Integer::intValue)
+						  .toArray();
+        return answer;
+    }
+}
