a702d78d7cd9bf7bd8267977456bcc812f797adc
지은 <107034832+6868x88@users.noreply.github.com>
2026-01-02 14:22:38 +0900

    260102_2.sql 610. Triangle Judgement

---
+++ Leetcode/260102_2.sql
@@ -0,0 +1,12 @@
+/*
+610. Triangle Judgement
+*/
+
+select x
+      , y
+      , z
+      ,case when x + y <= z then 'No' 
+            when y + z <= x then 'No'
+            when z + x <= y then 'No' 
+            else 'Yes' end as triangle 
+from Triangle
