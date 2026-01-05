87196d9211239da8d268706495505ac67b99971b
지은 <107034832+6868x88@users.noreply.github.com>
2026-01-05 10:22:54 +0900

    260105_1.sql 1517. Find Users With Valid E-Mails

---
+++ Leetcode/260105_1.sql
@@ -0,0 +1,8 @@
+/*
+1517. Find Users With Valid E-Mails
+*/
+
+/* Write your PL/SQL query statement below */
+select *
+from Users
+where REGEXP_LIKE(mail, '^[A-Za-z]+[A-Za-z0-9._-]*@leetcode\.com$')
